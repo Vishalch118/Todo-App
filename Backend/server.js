7e1a7e66123911c6295c121635fb0b64aa71d77c
Vishalch118 <vishal.ch22254@gmail.com>
2026-03-07 23:34:04 +0530

    fixed for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -10,12 +10,15 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+)
